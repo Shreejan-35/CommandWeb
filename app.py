4f52d28d2cc0348f66488b0d5896bf30527c3027
Shreejan <dolaishreejan@gmail.com>
2021-12-07 12:09:22 +0530

    Made the overview page

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ def home():
 def login():
     return "login, page"
 
+@app.route('/overview')
+def overview():
+    return render_template('overview.html')
+
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
