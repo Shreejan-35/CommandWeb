9c4fa91ad095b6ea0540652145aa64eb0cff8826
Shreejan <dolaishreejan@gmail.com>
2022-01-08 17:44:54 +0530

    Made the login page and create the database

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+#making databse and cinfiguring it
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(10), nullable = False)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return "login, page"
-
 @app.route('/overview')
 def overview():
     return render_template('overview.html')
 
-@app.route('/GetStared')
-def download():
-    return render_template('GetStared.html')
+@app.route('/GetStarted')
+def GetStarted():
+    return render_template('GetStarted.html')
 
-@app.route('/download')
-def download():
-    return render_template('download.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
