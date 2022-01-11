e7ef0d3a6aeb0c35c70ef9a3d5d8f1120db5a9ed
Shreejan <dolaishreejan@gmail.com>
2022-01-11 12:39:24 +0530

    replaced the sqlite with firebase

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from typing import final
+import pyrebase
 from flask import Flask, render_template, request, json
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 #making databse and cinfiguring it
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+config = {
+    "apiKey": "AIzaSyA10S3-yjJ-x7qAYnrbJc07gbMK17Xjkts",
+    "authDomain": "commandconsoleuser.firebaseapp.com",
+    "databaseURL": "https://commandconsoleuser-default-rtdb.firebaseio.com",
+    "projectId": "commandconsoleuser",
+    "storageBucket": "commandconsoleuser.appspot.com",
+    "messagingSenderId": "217715886011",
+    "appId": "1:217715886011:web:b68a9ef60117a02957669e",
+    "measurementId": "G-6DM8EW6RM2"
+}
 
-class Users(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(10), nullable=False)
-    password = db.Column(db.String(10), nullable = False)
-
-    def __repr__(self) -> str:
-        return f"{self.username} - {self.password}"
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+auth = firebase.auth()
 
 @app.route('/')
 def home():
@@ -28,48 +33,21 @@ def overview():
 def GetStarted():
     return render_template('GetStarted.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    IsUser = None
-    IsPassword = None
-    UserName = None
-    finalList= []
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        validUsername = Users.query.filter_by(username=username).first()
-        validPassword = Users.query.filter_by(password=password).first()
-        if(validUsername != None):
-       
-            if(validPassword != None):
-                IsUser = "True"
-                IsPassword = "True"
-                UserName = validUsername
-            else:
-                IsUser="False"
-                IsPassword="False"
-        else:
-            IsUser = "False"
-        
-        finalList.append(IsUser)
-        finalList.append(IsPassword)
-        finalList.append(username)
-        
-    return render_template('login.html', finallist = finalList)
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        users = Users(username=username, password=password)
-        db.session.add(users)
-        db.session.commit()
         
-    allUsers = Users.query.all()
-    print(allUsers)
-    return render_template('register.html', allUsers = allUsers)
+        if request.form['submit'] == "add":
+            username = request.form['username']
+            password = request.form['password']
+            db.child("users").child("usersList").child(username).push({username:password})
+        
+        elif request.form['submit'] == "delete":
+            username = request.form['username']
+            password = request.form['password']
+            db.child("users").child("usersList").child(username).remove()
 
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
