7c6f00d8ce9432f9d4c5aa9bfe0b4f8255810414
Shreejan <dolaishreejan@gmail.com>
2022-01-12 22:06:15 +0530

    done

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:54965e260fbeaf428448db42c015849c4e01aea20ca6651c849a91a42419bcbb
-size 1681
+from flask import Flask, render_template, request, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/overview')
+def overview():
+    return render_template('overview.html')
+
+@app.route('/GetStarted')
+def GetStarted():
+    return render_template('GetStarted.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
